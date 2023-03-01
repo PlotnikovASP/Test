cd60c0e6152a6622b84cb529b1e4ab7eb7dd58bf
Plotnikov Anton <test@mail.ru>
2023-03-01 21:03:11 +0300

    init

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,34 @@
+public class MainApp {
+    public static void main(String[] args) {
+        printColor();
+
+    }
+
+    public static void printColor() {
+        int value = 110;
+        if (value <= 0){
+            System.out.println("Красный");
+        }if (value > 0 && value < 100){
+            System.out.println("Желтый");
+        }if (value > 101 ){
+            System.out.println("Зеленый");
+    }
+}
+
+    //2hw
+/*
+    public static void checkSumSign(){
+        int a = 2;
+        int b = 3;
+        if ( a + b >= 0){
+            System.out.println(" Сумма положительная");
+
+        }else {
+            System.out.println("Сумма отрицательная");
+        }*/
+//1 hw
+    /* public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");*/
+}
